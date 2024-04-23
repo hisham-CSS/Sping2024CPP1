8190a32595c8a2db21429519e4b8671ad2ccf4de
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2024-04-23 11:02:06 -0400

    Lab 3 incomplete

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,32 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerController : MonoBehaviour
-{
+{ 
+    public bool TestMode;
+    //Movement Variables
     [SerializeField] private int speed;
+    [SerializeField] private int jumpForce = 3;
+
+    //Groundcheck
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask isGroundLayer;
+    [SerializeField] private float groundCheckRadius;
 
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         if (speed <= 0)
         {
             speed = 5;
-            Debug.Log("Speed Set To Default Value");
+            if (TestMode) Debug.Log("Speed Set To Default Value");
         }
 
-        rb = GetComponent<Rigidbody2D>();
-    }
+        if (jumpForce <= 0) 
+        {
+            jumpForce = 3;
+            if (TestMode) Debug.Log("Jump Force Set To Default Value");
+        }
 
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.02f;
+            if (TestMode) Debug.Log("Ground Check Radius Set To Default Value");
+        }
+
+        if (groundCheck == null)
+        {
+            GameObject obj = GameObject.FindGameObjectWithTag("GroundCheck");
+            if (obj != null)
+            {
+                groundCheck = obj.transform;
+                return;
+            }
+            GameObject newObj = new GameObject();
+            newObj.transform.SetParent(transform);
+            newObj.transform.localPosition = Vector3.zero;
+            newObj.name = "GroundCheck";
+            newObj.tag = newObj.name;
+            groundCheck = newObj.transform;
+            if (TestMode) Debug.Log("Ground Check Transform Created via Code - Did you forget to assign it in the inspector?");
+        } 
+    }
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
         Vector2 moveDirection = new Vector2(xInput * speed, rb.velocity.y);
-
         rb.velocity = moveDirection;
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        //Sprite Flipping
+        if (xInput != 0) sr.flipX = (xInput < 0);
+
+        anim.SetFloat("speed", Mathf.Abs(xInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        //if (xInput > 0 && sr.flipX || xInput < 0 && !sr.flipX)
+        //{
+        //    sr.flipX = !sr.flipX;
+        //}
     }
 }
